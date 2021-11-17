86f520425f69f5c5ade0fcc8853191d340d6eac4
adarshsinghbam <adarshsingh922007@gmail.com>
2021-11-17 22:01:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ScoreKeeper from "./ScoreKeeper";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ScoreKeeper />
+      </div>
+    );
+  }
+}
+
+export default App;
